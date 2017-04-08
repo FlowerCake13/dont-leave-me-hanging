5b98811ee2785b0500c72487a8e29bd86c567c68
FlowerCake13 <lminami23@punahou.edu>
2017-04-08 11:59:20 -1000

    Step 5 js
    Except last bullet

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l"
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l",
 "m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var wordBank = ["lindsay", "cake"];
+var wordBank = ["hi", "cake"];
 var emptyString = "";
 var emptyArr = [];
 
 function gameStart() {
-	//Finish Last 3 Bullets on 5...
+	var randomNum = Math.floor(Math.random() * (2-0) + 0);
+	document.getElementById('letters-container').innerHTML;
+	for (var i = 0; i < wordBank[randomNum].length; i++){
+		document.getElementById('word-container').innerHTML += "-";
+	}
 }
